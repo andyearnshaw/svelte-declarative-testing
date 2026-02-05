8fcd36a3e27b9c7fa81ed03e3331e3894c7fa2c5
Andy Earnshaw <andyearnshaw@gmail.com>
2026-02-05 13:29:50 +0000

    test: add basic example & test

---
+++ examples/basic/Basic.test.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Test, Describe, Check } from '../../src/components/testing-library';
+</script>
+
+<Describe label="Basic test">
+  {#snippet tests()}
+    <Test it="passes">
+      <button>Click me</button>
+
+      {#snippet checks()}
+        <Check
+          fn={({ getByRole }) => {
+            expect(getByRole('button', { name: 'Click me' })).not.toBe(null);
+          }}
+        />
+      {/snippet}
+    </Test>
+  {/snippet}
+</Describe>
---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitest/config';
+import { svelteTesting } from '@testing-library/svelte/vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import getPlugins from './src/plugins/vitest';
+
+export default defineConfig({
+  logLevel: 'warn',
+  plugins: [svelte(), svelteTesting(), getPlugins()],
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+    },
+    environment: 'happy-dom',
+    expect: {
+      requireAssertions: true,
+    },
+    globals: true,
+    include: ['examples/**/*.{test,spec}.svelte'],
+  },
+});
